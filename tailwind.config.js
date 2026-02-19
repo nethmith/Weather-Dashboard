64a6b630c952dc6b668b1b74674fdffdb10950e6
nethmith <nethmiherath100@gmail.com>
2026-02-19 10:59:48 +0530

    Fix dark mode toggle

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    darkMode: 'selector', // Use 'selector' for manual toggling
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
